0ac28401511df64a8b61d495aa22c8c3e50d57d1
hutch <hutch>
2009-07-29 21:19:35 +0000

    Forgot to add reincom.f

---
+++ reincom.f
@@ -0,0 +1,18 @@
+c********************************************************************
+c Left-over requirements for orbitinject.
+      real averein,adeficit
+c Put them into a common.
+      common /reinextra/averein,adeficit
+c Parameters used by orbitinject.
+      integer myid
+      parameter (myid=0)
+      integer nthsize
+      parameter (nthsize=201)
+
+c Reindiag parameters.
+      integer ndth,ndpsi
+      parameter (ndth=100,ndpsi=100)
+      real reinpos(ndth,ndpsi),cthtot(ndth),psitot(ndpsi)
+      real reincth(ndth),reinpsi(ndpsi)
+      real fv(ndth,3),vfv(ndth)
+      common /diagrein/reinpos,cthtot,psitot,reincth,reinpsi,fv,vfv
