2a637cf7794413bcdc87d6b8a51cd56c18cf317f
hutch <hutch>
2010-09-13 14:40:09 +0000

    ptaccom.f prior to non-uniform

--- ptaccom.f
+++ ptaccom.f
@@ -1,7 +1,7 @@
-      parameter (ndiag=200,mdims=3)
+      parameter (ndiag=100,mdims=3)
       real xr(3*mdims)
       real fv(ndiag,mdims)
       real px(ndiag,mdims)
-      real diagv(ndiag,mdims)
-      real diagx(ndiag,mdims)
-      common /cartdiag/fv,px,diagv,diagx
+      real diagv(ndiag,mdims),cumdiagv(ndiag,mdims)
+      real diagx(ndiag,mdims),cumdiagx(ndiag,mdims)
+      common /cartdiag/fv,px,diagv,diagx,cumdiagv,cumdiagx
