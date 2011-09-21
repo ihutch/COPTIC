96d9cb055278cba3e603cc23560f4375784a150c
hutch <hutch>
2011-09-21 20:51:41 +0000

    Increased griddecl.f x-dimension back to a reasonable number.

--- griddecl.f
+++ griddecl.f
@@ -1,4 +1,4 @@
 c Declarations of grid array parameters.
       integer ndims,na_m,na_i,na_j,na_k
       parameter (ndims=3)
-      parameter (na_m=200,na_i=4,na_j=200,na_k=200)
+      parameter (na_m=200,na_i=100,na_j=200,na_k=200)
