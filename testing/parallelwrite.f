185afebd14cccd452558f12ff4bc573c0afb7ced
Ian Hutchinson <ihutch@mit.edu>
2017-01-07 17:37:37 -0500

    Fix parallel write to accept a directory argument

--- testing/parallelwrite.f
+++ testing/parallelwrite.f
@@ -2,13 +2,20 @@ c Do parallel writing as a test of file systems access etc.
       include 'mpif.h'
 c Number of integers to write:
       parameter (iblen=100000)
-      character*20 filename
+      character*80 filename
+
+      if(iargc().eq.1)then
+         call getarg(1,filename)
+      else
+         filename=' '
+      endif
+      
       call MPI_INIT(ierr)
 
       call MPI_COMM_RANK( MPI_COMM_WORLD, myid, ierr )
       call MPI_COMM_SIZE( MPI_COMM_WORLD, numprocs, ierr )
-      filename='Written.'
-      write(filename(9:),'(i4.4)')myid
+      ilen=nbcat(filename,'T1Written.')
+      write(filename(lentrim(filename)+1:),'(i4.4)')myid
       write(*,*)'Writing to file: ',filename
       
       open(1,file=filename,status='unknown',err=101)
