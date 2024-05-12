02046972a001f674648d40abc55849fcc48f1c91
Ian Hutchinson <ian@ianhhutchinson.com>
2024-05-12 18:05:06 -0400

    Developed EnergyPhase analysis routine that takes pps files and calculates the field energy evolution as a function of time. It fits a growth rate by finding the peak of dln(E^2)/dt, and plots a slope on the energy vs time curve.

---
+++ analysis/EnergyPhase.f
@@ -0,0 +1,132 @@
+c Main program to get and plot phasespace data from file[s] on command line.
+c At the same time get the potential u, and calculate energy \int E^2dx
+      implicit none
+      include '../src/ndimsdecl.f'
+      include '../src/phasecom.f'
+      include '../src/partcom.f'
+      include '../accis/plotcom.h'
+      character*100 phasefilename
+      real x(npsbuf),u(npsbuf)!,uave(npsbuf)
+      integer nfiles,j,k,nsmooth,knmax,idk
+      parameter (nfiles=10000)
+      real energy(nfiles),time(nfiles),xlen,dlnen(nfiles),smooth(nfiles)
+      real t,slope,enl,enkn,enu,tknl,tknmax,tknu
+      real phirange,phirangeinit
+      parameter (phirangeinit=0.5)
+      integer i,ii,n,Np,Nave,Nastep,idone,irun
+      character*12 nlabel(2)
+      logical ldebug
+      data nlabel/' !Bn!di!d!@',' !Bn!de!d!@'/
+      data idone/0/irun/0/
+      ldebug=.false.
+      nsmooth=160
+      Nave=1
+      Nastep=1
+      phirange=phirangeinit
+      j=0
+      call pfset(3)
+      do i=1,iargc()
+         call getarg(i,phasefilename)
+         if(phasefilename(1:2).eq.'-N')then
+c Set the starting number of filewriting to be N
+            read(phasefilename(3:),*,err=2,end=2)Np
+            pfilno=Np
+            write(*,*)'Plot file number offset:',Np
+            goto 1
+ 2          write(*,*)'Garbled -N flag (needs number):',phasefilename
+            goto 1
+         endif
+         if(ldebug)write(*,*)phasefilename(1:50)
+         if(phasefilename(1:2).eq.'-A')then
+            read(phasefilename(3:),*,err=3,end=3)Nave
+            write(*,*)'Averaging potential over steps:',Nave
+            goto 1
+ 3          write(*,*)'Garbled -a flag (needs number):',phasefilename
+            goto 1
+         elseif(phasefilename(1:3).eq.'-ns')then
+            read(phasefilename(4:),*,err=6,end=6)nsmooth
+            write(*,*)'dln(energy)/dt smoothing over range',nsmooth
+            goto 1
+ 6          write(*,*)'Garbled -ns flag (needs number):',phasefilename
+            goto 1
+         elseif(phasefilename(1:2).eq.'-d')then
+            ldebug=.not.ldebug
+         elseif(phasefilename(1:2).eq.'-h')then
+            goto 4
+         elseif(phasefilename(1:2).eq.'-q')then
+            irun=1
+            call pfset(-3)
+         else
+            if(idone.gt.0)call prtend(' ')
+            n=npsbuf
+            j=j+1
+            if(j.gt.nfiles)then
+               write(*,*)'File number exceeds nfiles',nfiles
+               goto 5
+            endif
+            call phaseread(phasefilename,n,x,u,t)
+            energy(j)=0.
+            xlen=abs(x(n)-x(1))
+            do ii=2,n
+               energy(j)=energy(j)+(u(ii)-u(ii-1))**2/(x(ii)-x(ii-1))
+            enddo
+            energy(j)=energy(j)/xlen  ! Thus energy density
+            time(j)=t
+         endif
+ 1       continue
+      enddo
+      if(j.eq.0)goto 4
+ 5    continue  
+! Calculate the time derivative of ln(energy)
+      do i=2,j  
+         dlnen(i)=(alog(energy(i))-alog(energy(i-1)))
+     $        /(time(i)-time(i-1))
+      enddo
+! Smooth dlnen over a range of nsmooth+1 points. (Should be adjustable.)
+! And find the peak smoothed value. 
+      knmax=1
+      do i=1+nsmooth/2,j-nsmooth/2  ! Triangular smoothing
+         smooth(i)=0.
+         do k=i-nsmooth/2,i+nsmooth/2
+            smooth(i)=smooth(i)+dlnen(k)*(1-abs(k-i)/(nsmooth/2.))
+         enddo
+         smooth(i)=smooth(i)/(nsmooth/2.)
+         if(smooth(i).gt.smooth(knmax))knmax=i
+      enddo
+! Now we have the steepest slope of the smoothed dlnen.
+      slope=smooth(knmax)
+! Get the ends of the straight line with this slope to draw
+      idk=nint(.6*knmax)
+      tknmax=time(knmax)
+      tknu=time(knmax+idk)
+      tknl=time(knmax-idk)
+      enkn=energy(knmax)
+      enl=enkn*exp(-slope*(tknmax-tknl))
+      enu=enkn*exp(-slope*(tknmax-tknu))
+      write(*,'(a,f10.5a,f7.2)')'Peak growth rate:',slope
+     $ ,'   Growth time:',1/slope
+      if(ldebug)then
+         write(*,*)'knmax=',knmax,' idk=',idk,' slope=',slope
+         write(*,*)'tknl,tknu',tknl,tknu
+         write(*,*)'enl ,enu ',enl,enu
+         call autoplot(time,smooth,j) ! Check the smoothed log slope.
+         call axlabels('time','smoothed dln<E!u2!u>/dt')
+         call pltend
+      endif
+! Do the growth plot
+      call lautoplot(time,energy,j,.false.,.true.)
+      call axlabels('time','field energy density <E!u2!u>')
+! Overplot the slope line
+      call color(4)
+      call polyline([tknl,tknu],[enl,enu],2)
+      call pltend
+      call exit
+ 4    continue
+      write(*,*)'Obtain the evolution of the field energy from',
+     $     ' one dimensional pps files'
+      write(*,*)'Usage:  EnergyPhase [Options] file1 [file2 ....]'
+      write(*,*)'Options: -A<nnn> average-number',' -N starting-number'
+      write(*,*)' -ns<nnn> smoothing range -q no screen plots or halts.'
+      write(*,*)' -d toggle debugging'
+      end
+ 
