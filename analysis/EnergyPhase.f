a7a126f30e9d2975202f733fac0af1900a6d7cd4
Ian Hutchinson <ian@ianhhutchinson.com>
2024-05-13 10:11:01 -0400

    Added a contour plot of phi on the t,x plane to EnergyPhase.

--- analysis/EnergyPhase.f
+++ analysis/EnergyPhase.f
@@ -1,4 +1,4 @@
-c Main program to get and plot phasespace data from file[s] on command line.
+c Main program to get (not plot) phasespace data from file[s] on command line.
 c At the same time get the potential u, and calculate energy \int E^2dx
       implicit none
       include '../src/ndimsdecl.f'
@@ -7,9 +7,11 @@ c At the same time get the potential u, and calculate energy \int E^2dx
       include '../accis/plotcom.h'
       character*100 phasefilename
       real x(npsbuf),u(npsbuf)!,uave(npsbuf)
-      integer nfiles,j,k,nsmooth,knmax,idk
-      parameter (nfiles=10000)
-      real energy(nfiles),time(nfiles),xlen,dlnen(nfiles),smooth(nfiles)
+      integer j,k,nsmooth,knmax,idk,nxmax,ixstep,nxua,icl,icsw
+      parameter (nxmax=512)
+      real energy(npsbuf),time(npsbuf),xlen,dlnen(npsbuf),smooth(npsbuf)
+      real uarray(npsbuf,nxmax),xuarray(nxmax),worka(npsbuf,nxmax)
+      real umin,umax,zclv(2)
       real t,slope,enl,enkn,enu,tknl,tknmax,tknu
       real phirange,phirangeinit
       parameter (phirangeinit=0.5)
@@ -27,8 +29,10 @@ c At the same time get the potential u, and calculate energy \int E^2dx
       call pfset(3)
       do i=1,iargc()
          call getarg(i,phasefilename)
+         if(ldebug)write(*,*)phasefilename(1:50)
+! Deal with possible switches
          if(phasefilename(1:2).eq.'-N')then
-c Set the starting number of filewriting to be N
+c Set the starting number of plot file writing to be N
             read(phasefilename(3:),*,err=2,end=2)Np
             pfilno=Np
             write(*,*)'Plot file number offset:',Np
@@ -36,7 +40,6 @@ c Set the starting number of filewriting to be N
  2          write(*,*)'Garbled -N flag (needs number):',phasefilename
             goto 1
          endif
-         if(ldebug)write(*,*)phasefilename(1:50)
          if(phasefilename(1:2).eq.'-A')then
             read(phasefilename(3:),*,err=3,end=3)Nave
             write(*,*)'Averaging potential over steps:',Nave
@@ -57,14 +60,24 @@ c Set the starting number of filewriting to be N
             irun=1
             call pfset(-3)
          else
-            if(idone.gt.0)call prtend(' ')
-            n=npsbuf
+! Not a known switch. Interpret as a file name, and read it.
             j=j+1
-            if(j.gt.nfiles)then
-               write(*,*)'File number exceeds nfiles',nfiles
+            if(j.gt.npsbuf)then
+               write(*,*)'File number exceeds npsbuf',npsbuf
                goto 5
             endif
+            n=npsbuf
             call phaseread(phasefilename,n,x,u,t)
+! Store in possibly limited x-range x,u versus time arrays.
+            ixstep=1+(n-1)/nxmax
+            k=0
+            do ii=1,n,ixstep
+               k=k+1
+               uarray(j,k)=u(ii)
+               if(j.eq.1)xuarray(k)=x(ii)
+            enddo
+            nxua=k
+! Find total average energy density at this time.
             energy(j)=0.
             xlen=abs(x(n)-x(1))
             do ii=2,n
@@ -75,6 +88,7 @@ c Set the starting number of filewriting to be N
          endif
  1       continue
       enddo
+      write(*,'(10f8.2)')(xuarray(k),k=1,nxua) ! Check xuarray.
       if(j.eq.0)goto 4
  5    continue  
 ! Calculate the time derivative of ln(energy)
@@ -113,13 +127,34 @@ c Set the starting number of filewriting to be N
          call axlabels('time','smoothed dln<E!u2!u>/dt')
          call pltend
       endif
+      call multiframe(2,1,1)
 ! Do the growth plot
       call lautoplot(time,energy,j,.false.,.true.)
       call axlabels('time','field energy density <E!u2!u>')
+      call axis2
 ! Overplot the slope line
       call color(4)
       call polyline([tknl,tknu],[enl,enu],2)
+      call color(15)
+!      call pltend
+! Put contour plot of uarray(t,x) here.
+      call pltinit(time(1),time(j),xuarray(1),xuarray(nxua))
+      call minmax2(uarray,npsbuf,j,nxua,umin,umax)
+      zclv(1)=umin/2
+      zclv(2)=umax/2
+      icl=2
+      icsw=1+16+32
+!      call blueredgreenwhite()
+      call contourl(uarray,worka,npsbuf,j,nxua,zclv,icl,
+     $    time,xuarray,icsw)
+      call color(2)
+      call axis
+      call axis2
+      call axlabels('time','x')
+      call gradlegend(zclv(1),zclv(2),1.04,0.,1.04,.9,.02,.true.)
+      call legendline(1.03,.95,258,'!Af!@')
       call pltend
+      call multiframe(0,0,0)
       call exit
  4    continue
       write(*,*)'Obtain the evolution of the field energy from',
